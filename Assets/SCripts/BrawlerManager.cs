6bf57bb5059cc2e97aaf697d0e6424a19719779f
sandy410 <sandeep410@rediffmail.com>
2022-01-06 15:38:55 +0530

    1st Commit

---
+++ Assets/SCripts/BrawlerManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Invector.vCamera;
+using Invector.vCharacterController;
+
+public class BrawlerManager : MonoBehaviour
+{
+   
+   public enum CameraAngle {RightAngle, LeftAngle };
+    CameraAngle CurrentCameraSide;
+  
+    public vThirdPersonInput tpInput;
+    vLockOn lockonmanager;
+    Animator anim;
+    vThirdPersonController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        cc = GetComponent<vThirdPersonController>();
+        if (tpInput == null)
+            tpInput = GetComponent<vThirdPersonInput>();
+        lockonmanager = GetComponent<vLockOn>();
+        CurrentCameraSide = CameraAngle.RightAngle;
+        Invoke("LockoN", 1.0F);
+        
+      
+    }
+
+    public void LockoN()
+    {
+        lockonmanager.StartLockOn();
+    }
+
+    public void ChangeCameraAngle()
+    {
+        if (CurrentCameraSide == CameraAngle.RightAngle)
+        {
+            tpInput.ChangeCameraState("LeftStrafing");
+            CurrentCameraSide = CameraAngle.LeftAngle;
+
+        }
+        else
+        {
+            tpInput.ResetCameraState();
+            CurrentCameraSide = CameraAngle.RightAngle;
+        }
+
+
+    }
+
+    public void Exhausted()                 // When your char runs out of stamina
+    {
+        anim.SetTrigger("Tired");
+    }
+    
+
+    public void LockInput()
+    {
+        print("LockInpiut");
+        GetComponent<vMeleeCombatInput>().SetLockMeleeInput(true);
+    }
+    public void UnLockInput()
+    {
+        print("Unlock");
+        GetComponent<vMeleeCombatInput>().SetLockMeleeInput(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            ChangeCameraAngle();
+
+        if (Input.GetKeyDown(KeyCode.B))
+            GetComponent<Animator>().SetTrigger("Tired");
+        if(cc.IsAnimatorTag("Taunt"))
+        {
+            GetComponent<vMeleeCombatInput>().lockInput = true;
+        }
+
+
+    }
+}
